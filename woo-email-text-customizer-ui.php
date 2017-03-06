a870f5efacc7adf0c0d4ffcc5c2d92e6148fe46e
Sheraz Ahmed <sherazahmed93@gmail.com>
2017-03-06 23:13:40 +0500

    started working on UI

--- woo-email-text-customizer-ui.php
+++ woo-email-text-customizer-ui.php
@@ -27,27 +27,23 @@ class WETCUI {
 	 * The Function will output the page
 	 * */
 	public function templateCustomizerPage() {
-		echo '<h1 class="wp-heading-inline">Email Template Text Customizer</h1>';
-		?>
-				<select name="email-template" id="wetc-email-template">
-					<?php
-					foreach (new DirectoryIterator( plugin_dir_path( __FILE__ ) . "assets/email-templates") as $file) {
-						if ($file->isFile()) {
-							print '<option>'. $file->getFilename() . '</option>';
-						}
-					}
-					?>
-				</select>
-		<?php
+		echo '<div class="wrap">';
+		echo '<h1 class="wp-heading-inline">Email Template Text Customizer </h1>';
+		echo '<select name="email-template" id="wetc-email-template">';
+		foreach ( new DirectoryIterator( plugin_dir_path( __FILE__ ) . "assets/email-templates" ) as $file ) {
+			if ( $file->isFile() ) {
+				print '<option>' . $file->getFilename() . '</option>';
+			}
+		}
+		echo '</select>';
+		echo '</div>';
 	}
 
 	private function pluginHeader() {
+
 		$headsAttribs = "";
-		$headsAttribs .= '<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>';
-		$headsAttribs .= '<script src="http://netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>';
-		$headsAttribs .= '<script src="http://code.jquery.com/ui/1.10.3/jquery-ui.js"></script>';
+
 
 		echo $headsAttribs;
-
 	}
 }
