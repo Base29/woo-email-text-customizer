dc4a27c91215cdd620c03f5202b93cc484954895
Sheraz Ahmed <sherazahmed93@gmail.com>
2017-03-07 17:58:10 +0500

    Fetched Email templates using WC_Email Instance

--- woo-email-text-customizer-ui.php
+++ woo-email-text-customizer-ui.php
@@ -9,12 +9,25 @@ class WETCUI {
 	/**
 	 * WETCUI constructor.
 	 */
+	private $emails = null;
+	
 	public function __construct() {
+		add_action('init', array($this, 'load') );
 		add_action( 'admin_menu', array( $this, 'adminMenu' ) );
-		add_action( 'wp_head', array( $this, 'pluginHeader' ) );
-
+		add_action( 'admin_head', array( $this, 'pluginHeader' ) );
+		
 	}
-
+	
+	public function load(){
+		if( class_exists('WC_Emails') ){
+			$wc_emails = WC_Emails::instance();
+			$emails = $wc_emails->get_emails();
+			if( !empty($emails) )
+				$this->emails = $emails;
+		}
+		
+	}
+	
 	/**
 	 * The method will show menu in the WooCommerce
 	 */
@@ -22,28 +35,50 @@ class WETCUI {
 		add_submenu_page( 'woocommerce', 'Email Template Text  Customizer', 'Email Template  Text Customizer', 'manage_options',
 			'wetc', array( $this, 'templateCustomizerPage' ) );
 	}
-
+	
 	/**
 	 * The Function will output the page
 	 * */
 	public function templateCustomizerPage() {
-		echo '<div class="wrap">';
-		echo '<h1 class="wp-heading-inline">Email Template Text Customizer </h1>';
-		echo '<select name="email-template" id="wetc-email-template">';
-		foreach ( new DirectoryIterator( plugin_dir_path( __FILE__ ) . "assets/email-templates" ) as $file ) {
-			if ( $file->isFile() ) {
-				print '<option>' . $file->getFilename() . '</option>';
-			}
-		}
-		echo '</select>';
-		echo '</div>';
+		?>
+		<div class="wrap">
+			<h1 class="">Email Template Text Customizer </h1>
+			<h4>Please enter "#%d" for where you want the price to appear.</h4>
+			<h4>Please enter "%s" for where you want the name to appear.</h4>
+			<hr/>
+			<div class="">
+				<table class="widefat fixed" cellspacing="0">
+					
+					<?php
+					foreach ( $this->emails as $index => $email ) { ?>
+						<tr>
+							<td><?php echo $email->title; ?></td>
+						</tr>
+					<?php }
+					
+					/*foreach (
+						new DirectoryIterator( plugin_dir_path( __FILE__ ) . "assets/email-templates" ) as
+						$file
+					) { */?><!--
+						<tr>
+							<?php
+/*							if ( $file->isFile() ) {
+								print '<td>' . $file->getFilename() . '</td>';
+								print '<td><textarea></textarea></td>';
+							} */?>
+						</tr>
+						--><?php
+/*					}*/
+					?>
+				</table>
+			</div>
+		</div>
+		<?php
 	}
-
-	private function pluginHeader() {
-
-		$headsAttribs = "";
-
-
-		echo $headsAttribs;
+	
+	public
+	function pluginHeader() {
+		wp_enqueue_style( 'wetc_styles', plugins_url( "assets/css/style.css", __FILE__ ) );
 	}
+	
 }
